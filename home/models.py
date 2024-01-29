1b5fe63633abf4b24960e667c77c154aa0db994b
RABIN123DAHAL <rabindahal673@gmail.com>
2024-01-29 16:06:31 +0545

    models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class AboutMe(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    image = models.ImageField(upload_to='media')
+
+
+
+    #def __str__(self):
+        #return self.name
+
+class Service(models.Model):
+        name = models.CharField(max_length=300)
+        description = models.TextField()
+        logo = models.CharField(max_length=300)
+
+
+class Feedback(models.Model):
+        name = models.CharField(max_length=300)
+        post = models.CharField(max_length=300)
+        comment = models.TextField()
+        image = models.ImageField(upload_to='media')
+ class Address(models.Model):
+     phone = models.
